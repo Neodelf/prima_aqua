55ca622d4c445e0b5739335fe92aac1672d533ac
Neodelf <neodelf@gmail.com>
2015-01-13 16:33:44 +0300

    added pomps

--- app/admin/pomp.rb
+++ app/admin/pomp.rb
@@ -1,18 +1,17 @@
 ActiveAdmin.register Pomp do
+  permit_params :title, :description, :image, :price, :seo_title, :seo_description, :seo_keywords
 
+  form partial: 'admin/pomps/form.html.haml'
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  permit_params :title, :description, :image, :price
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
+  show do |pomp|
+    attributes_table do
+      [:title, :description, :price, :seo_title, :seo_description, :seo_keywords].each do |attr|
+        row attr.to_sym
+      end
+      row :image do
+        image_tag pomp.image.url
+      end
+    end
+    active_admin_comments
+  end
 end
