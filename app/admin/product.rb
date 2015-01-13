e91b5f822cf079c26432e55c4ffd5a912346b60b
Neodelf <neodelf@gmail.com>
2015-01-13 16:50:51 +0300

    added products

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,18 +1,17 @@
 ActiveAdmin.register Product do
-
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  permit_params :title, :image, :price
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
-
+  form partial: 'admin/products/form'
+  show do |product|
+    attributes_table do
+      row :title
+      row :price
+      row :seo_title
+      row :seo_description
+      row :seo_keywords
+      row :image do
+        image_tag(product.image)
+      end
+    end
+    active_admin_comments
+  end
+  permit_params :title, :image, :price, :seo_title, :seo_description, :seo_keywords
 end
