0364cdfbeb169c92834f28dd5a8b027e0e706d06
lvl0nax <lvl0nax@gmail.com>
2015-04-11 23:06:23 +0300

    adding devise files

--- db/migrate/20150410171030_add_devise_to_users.rb
+++ db/migrate/20150410171030_add_devise_to_users.rb
@@ -1,7 +1,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
-      ## Database authenticatable
       t.string :name
       t.string :phone
       t.string :address
@@ -9,8 +8,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
   end
 
   def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
   end
 end
