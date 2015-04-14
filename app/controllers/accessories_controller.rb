e24f93b91226f71f6dfc7e692aa4439fde2720bd
Andrew Molchanov <amolchanov@ailabs.ru>
2015-04-14 22:29:18 +0300

    some little fixes

--- app/controllers/accessories_controller.rb
+++ app/controllers/accessories_controller.rb
@@ -1,5 +1,10 @@
 class AccessoriesController < ApplicationController
   def index
-    @accessories = Accessory.order(:id)
+    @accessories = Accessory.all
+    @accessories = if params[:order].present? && params[:order] == 'price'
+                     @accessories.order(:price)
+                   else
+                     @accessories.order(:id)
+                   end
   end
 end
