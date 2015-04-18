3a8c08ef3d8b5b2f3b9983beabd3ca5cdc247e30
lvl0nax <lvl0nax@gmail.com>
2015-04-18 20:59:34 +0300

    fix prices form

--- app/admin/price.rb
+++ app/admin/price.rb
@@ -1,16 +1,7 @@
 ActiveAdmin.register Price do
   permit_params :aqua_id, :value, :start_count, :end_count, :volume_id
 
-  form do |f|
-    f.inputs do
-      f.input :aqua_id, label: 'Aqua', as: :select, collection: Aqua.all.map{|a| ["#{a.name}", a.id]}
-      f.input :value
-      f.input :volume_id, label: 'Volume', as: :select, collection: option_groups_from_collection_for_select(Aqua.all, :volumes, :name, :id, :value, @price.volume_id)
-      f.input :start_count
-      f.input :end_count
-      f.actions
-    end
-  end
+  form partial: 'admin/prices/form.html.haml'
 
   index do
     selectable_column
