ffde642f43c5dab0f446fe27104e17aceffffc00
lvl0nax <lvl0nax@gmail.com>
2015-04-27 22:06:08 +0300

    adding files for aquas

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register User do
 
+  form partial: 'admin/users/form'
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
-
+  controller do
+    def permitted_params
+      params['user']['customer_type'] = params['user']['customer_type'].to_i
+      params.permit!
+    end
+  end
 
 end
