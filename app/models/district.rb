236212663bc737b08f312174d8bee87f96def7f3
lvl0nax <lvl0nax@gmail.com>
2015-05-05 19:13:48 +0300

    bugs fixes

--- app/models/district.rb
+++ app/models/district.rb
@@ -13,14 +13,14 @@ class District < ActiveRecord::Base
   private
   def set_coord
     if self.lat.blank? && self.lon.blank?
-      url = "http://geocode-maps.yandex.ru/1.x/?format=json&geocode=#{d.name.split.join('+')}&ll=30.327541,59.936952&spn=2,2&rspn=1"
+      url = "http://geocode-maps.yandex.ru/1.x/?format=json&geocode=#{self.name.split.join('+')}&ll=30.327541,59.936952&spn=2,2&rspn=1"
       response = JSON.parse(Net::HTTP.get_response(URI.parse(URI.encode(url))).body)
-      d.lon, d.lat = if response['response']['GeoObjectCollection']['featureMember'].present?
+      self.lon, self.lat = if response['response']['GeoObjectCollection']['featureMember'].present?
                        response['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'].split
                      else
                        [0,0]
                      end
-      d.save
+      self.save
     end
   end
 end
