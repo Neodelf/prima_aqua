39feb3b53501bea292b7fb9ab14fca6d4a347e36
lvl0nax <lvl0nax@gmail.com>
2015-05-05 19:45:03 +0300

    fixes registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :update_sanitized_params, if: :devise_controller?
 
   def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation,:address, :phone)}
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation,:address, :phone, :customer_type)}
   end
 end
